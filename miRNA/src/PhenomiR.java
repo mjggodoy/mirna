6a2264caa99873645859e7f6003eaca7588a6ca5
Esteban López Camacho <esteban@lcc.uma.es>
2013-10-22 10:04:22 +0200

    Commit inicial. :)

---
+++ miRNA/src/PhenomiR.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+import beans.PhenomirBean;
+
+public class PhenomiR {
+
+	public static void main(String args[]) throws FileNotFoundException {
+
+		FileReader fr = new FileReader("C:/Users/usuario/Desktop/NewSearchingLine/phenomiR/phenomir2.0.txt");
+		BufferedReader br = new BufferedReader(fr);
+
+		PhenomirBean phenomir = new PhenomirBean();
+
+		//			 phenomir.setPhenomicid(sc.next());
+		//			String pua = phenomir.getPhenomicid();
+
+		String line;
+		int count;
+		try {
+
+			while((line=br.readLine())!=null) {
+
+				StringTokenizer st = new StringTokenizer(line, "\t");
+
+				if (st.countTokens() == 10) {
+					String field1=st.nextToken();
+					System.out.println(field1);
+					String field2=st.nextToken();
+					System.out.println(field2);
+					String field3=st.nextToken();
+					System.out.println(field3);
+					String field4=st.nextToken();
+					System.out.println(field4);
+					String field5=st.nextToken();
+					System.out.println(field5);
+					String field6=st.nextToken();
+					System.out.println(field6);
+					String field7=st.nextToken();
+					System.out.println(field7);
+					String field8=st.nextToken();
+					System.out.println(field8);
+					String field9=st.nextToken();
+					System.out.println(field9);
+					String field10=st.nextToken();
+					System.out.println(field10);
+				} else {}
+
+				
+			}
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+}
+	
---
+++ miRNA/src/beans/PhenomirBean.java
@@ -0,0 +1,41 @@
+package beans;
+
+public class PhenomirBean {
+	
+	//phenomicid	pmid	disease	diseasesub_id	class	miRNA	accession	expression	foldchangemin	foldchangemax	id	name	method
+	
+	private String phenomicid;
+	private String pmid;
+	private String disease;
+	
+	public PhenomirBean() {}
+
+	public String getPhenomicid() {
+		return phenomicid;
+	}
+
+	public void setPhenomicid(String phenomicid) {
+		this.phenomicid = phenomicid;
+	}
+
+	public String getPmid() {
+		return pmid;
+	}
+
+	public void setPmid(String pmid) {
+		this.pmid = pmid;
+	}
+
+	public String getDisease() {
+		return disease;
+	}
+
+	public void setDisease(String disease) {
+		this.disease = disease;
+	};
+	
+	
+	
+	
+
+}
