76250e82f9a9a56d51886a5ce8ccde8e49318b80
Esteban López Camacho <estebanpua@gmail.com>
2014-06-25 11:55:28 +0200

    Extensión del bean de miRNA.

--- miRNA/src/beans/MiRna.java
+++ miRNA/src/beans/MiRna.java
@@ -1,8 +1,7 @@
 package beans;
 
-public class MiRna{
+public class MiRna extends ModelClass {
 	
-
 	private String name;
 	private String accessionNumber;
 	private String subName;
@@ -14,6 +13,20 @@ public class MiRna{
 	
 	public MiRna() {}
 	
+	public MiRna(int id, String name, String accessionNumber, String subName,
+			String provenance, String chromosome, String version,
+			String sequence, String newName) {
+		super(id);
+		this.name = name;
+		this.accessionNumber = accessionNumber;
+		this.subName = subName;
+		this.provenance = provenance;
+		this.chromosome = chromosome;
+		this.version = version;
+		this.sequence = sequence;
+		this.newName = newName;
+	}
+
 	public String getVersion() {
 		return version;
 	}
@@ -67,20 +80,41 @@ public class MiRna{
 		return subName;
 	}
 
-
 	public void setSubName(String subName) {
 		this.subName = subName;
 	}
-
 
 	public String getProvenance() {
 		return provenance;
 	}
 
-
 	public void setProvenance(String provenance) {
 		this.provenance = provenance;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MiRna other = (MiRna) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	
 }
---
+++ miRNA/src/beans/ModelClass.java
@@ -0,0 +1,21 @@
+package beans;
+
+public class ModelClass {
+	
+	protected int id;
+	
+	public ModelClass() {}
+	
+	public ModelClass(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
