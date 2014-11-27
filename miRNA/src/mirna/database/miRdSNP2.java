ef112fb00588587ef8fbecfade7716f4d672833b
María Jesús García Godoy <mjggodoy@gmail.com>
2014-11-27 15:10:13 +0100

    mirdSNP2 cambiado

--- miRNA/src/mirna/database/miRdSNP2.java
+++ miRNA/src/mirna/database/miRdSNP2.java
@@ -10,7 +10,9 @@ import java.sql.Statement;
 
 import mirna.beans.Disease;
 import mirna.beans.ExpressionData;
+import mirna.beans.Gene;
 import mirna.beans.MiRna;
+import mirna.beans.SNP;
 import mirna.exception.MiRnaException;
 
 import org.apache.commons.lang.StringUtils;
@@ -117,40 +119,31 @@ public class miRdSNP2 extends miRdSNP {
 			rs.next();
 			// CAMBIAR ESTO:
 			
-			String phenomicid = rs.getString("phenomicid");
-			String pmid = rs.getString("pmid");
-			String diseaseField = rs.getString("disease").toLowerCase().trim();
-			String diseaseClass = rs.getString("class").toLowerCase().trim();
-			String mirna = rs.getString("miRNA").toLowerCase().trim();
-			String accession = rs.getString("accession").toLowerCase().trim();
-			String evidence = rs.getString("expression");
-			String foldchangemin = rs.getString("foldchangemin");
-			String foldchangemax = rs.getString("foldchangemax");
-			String studyDesign = rs.getString("name");
-			String method = rs.getString("method");
-			
-			
-			MiRna miRna = new MiRna();
-			miRna.setName(mirna);
-			miRna.setAccessionNumber(accession);
+			String ref_seq = rs.getString("refseq").toLowerCase().trim();
+			String gene_name = rs.getString("gene").toLowerCase().trim();
+			String mirna_name = rs.getString("mirna").toLowerCase().trim();
+			String snp_id = rs.getString("snp_id").toLowerCase().trim();
+			String disease_name = rs.getString("disease").toLowerCase().trim();
+
 			
 			Disease disease = new Disease();
-			disease.setName(diseaseField);
-			disease.setDiseaseClass(diseaseClass);
+			disease.setName(disease_name);
 			
-			ExpressionData ed = new ExpressionData();
-			ed.setProvenanceId(phenomicid);
-			ed.setPubmedId(pmid);
-			ed.setEvidence(evidence);
-			ed.setFoldchangeMin(foldchangemin);
-			ed.setFoldchangeMax(foldchangemax);
-			ed.setStudyDesign(studyDesign);
-			ed.setMethod(method);
-			ed.setProvenance("PhenomiR");
+			Gene gene = new Gene();
+			gene.setName(gene_name);
+			gene.setGeneId(ref_seq);
 			
-			System.out.println(miRna);
+			MiRna mirna = new MiRna();
+			mirna.setName(mirna_name);
+			
+			SNP snp = new SNP();
+			snp.setSNPid(snp_id);
+			
+			
 			System.out.println(disease);
-			System.out.println(ed);
+			System.out.println(snp);
+			System.out.println(gene);
+
 			
 			// FIN DE CAMBIAR ESTO
 			
