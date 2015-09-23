a61f75fd3ac5090cccd9510c2d6aa4a216e7404b
Esteban López Camacho <esteban@lcc.uma.es>
2015-09-23 17:36:49 +0200

    Preparados Microcosm y MicroTCdsData

--- miRNA/src/mirna/database/Microcosm.java
+++ miRNA/src/mirna/database/Microcosm.java
@@ -5,20 +5,17 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import mirna.beans.Gene;
 import mirna.beans.InteractionData;
 import mirna.beans.MiRna;
 import mirna.exception.MiRnaException;
-import mirna.utils.HibernateUtil;
 import mirna.beans.Target;
 import mirna.beans.Transcript;
 
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 /**
@@ -27,11 +24,15 @@ import org.hibernate.criterion.Restrictions;
  * @author Esteban López Camacho
  *
  */
-public class Microcosm extends MirnaDatabase {
+public class Microcosm extends NewMirnaDatabase {
 	
 	private final String tableName = "microcosm_homo_sapiens";
 	
 	public Microcosm() throws MiRnaException { super(); }
+	
+	protected String getTableName() {
+		return tableName;
+	}
 	
 	public void insertInTable(String csvInputFile) throws Exception {
 		
@@ -114,160 +115,114 @@ public class Microcosm extends MirnaDatabase {
 	}
 	
 	@Override
-	public void insertIntoSQLModel() throws Exception {
-
-		Connection con = null;
-		Statement stmt = null;
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Transaction tx = session.beginTransaction();
+	protected void processRow(Session session, ResultSet rs) throws Exception {
 		
-		try {
-			System.out.println(dbUrl);
-			System.out.println(dbUser);
-			System.out.println(dbPassword);
-			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-			stmt = (Statement) con.createStatement();
-			stmt.setFetchSize(Integer.MIN_VALUE);
+		String seq = rs.getString("seq");
+		String method = rs.getString("method");
+		String feature = rs.getString("feature").toLowerCase().trim();
+		String chromosome = rs.getString("chr").toLowerCase().trim();
+		String start = rs.getString("start").toLowerCase().trim();
+		String end = rs.getString("end").toLowerCase().trim();
+		String strand = rs.getString("strand").toLowerCase().trim();
+		String phase = rs.getString("phase");
+		String score = rs.getString("score");
+		String pvalue_og = rs.getString("pvalue_og");
+		String transcriptId = rs.getString("transcript_id");
+		String externalName = rs.getString("external_name");
 
-			// our SQL SELECT query.
-			// if you only need a few columns, specify them by name instead of
-			// using "*"
-			String query = "SELECT * FROM " + tableName;
-			System.out.println("STARTING: " + query);
+		MiRna miRna = new MiRna();
+		miRna.setName(seq);
 
-			// execute the query, and get a java resultset
-			ResultSet rs = stmt.executeQuery(query);
+		InteractionData id = new InteractionData();
+		id.setMethod(method);
+		id.setFeature(feature);
+		id.setPhase(phase);
+		id.setScore(score);
+		id.setPvalue_og(pvalue_og);
+		id.setProvenance("Microcosm");
 
-			// iterate through the java resultset
-			
-			int count = 0;
-			while(rs.next() && count <1){
-			// CAMBIAR ESTO:
+		Target target = new Target();
+		target.setChromosome(chromosome);
+		target.setPolarity(strand);
+		target.setBinding_site_start(start);
+		target.setBinding_site_end(end);
 
-			String seq = rs.getString("seq");
-			String method = rs.getString("method");
-			String feature = rs.getString("feature").toLowerCase().trim();
-			String chromosome = rs.getString("chr").toLowerCase().trim();
-			String start = rs.getString("start").toLowerCase().trim();
-			String end = rs.getString("end").toLowerCase().trim();
-			String strand = rs.getString("strand").toLowerCase().trim();
-			String phase = rs.getString("phase");
-			String score = rs.getString("score");
-			String pvalue_og = rs.getString("pvalue_og");
-			String transcriptId = rs.getString("transcript_id");
-			String externalName = rs.getString("external_name");
-
-			MiRna miRna = new MiRna();
-			miRna.setName(seq);
-
-			InteractionData id = new InteractionData();
-			id.setMethod(method);
-			id.setFeature(feature);
-			id.setPhase(phase);
-			id.setScore(score);
-			id.setPvalue_og(pvalue_og);
-			id.setProvenance("Microcosm");
-
-			Target target = new Target();
-			target.setChromosome(chromosome);
-			target.setPolarity(strand);
-			target.setBinding_site_start(start);
-			target.setBinding_site_end(end);
-
-			Transcript transcript = new Transcript();
-			transcript.setTranscriptID(transcriptId);
-			
-			Gene gene = new Gene();
-			gene.setGeneId(externalName);
+		Transcript transcript = new Transcript();
+		transcript.setTranscriptID(transcriptId);
+		
+		Gene gene = new Gene();
+		gene.setGeneId(externalName);
 
 
-			// Inserta MiRna (o recupera su id. si ya existe)
+		// Inserta MiRna (o recupera su id. si ya existe)
 
-			Object oldMiRna = session.createCriteria(MiRna.class)
-					.add(Restrictions.eq("name", miRna.getName()))
-					.uniqueResult();
-			if (oldMiRna == null) {
-				System.out.println("SALVANDO:");
-				System.out.println(miRna);
-				session.save(miRna);
-				session.flush(); // to get the PK
-			} else {
-				MiRna miRnaToUpdate = (MiRna) oldMiRna;
-				miRnaToUpdate.update(miRna);
-				session.update(miRnaToUpdate);
-				miRna = miRnaToUpdate;
-			}
-			
-			Object oldGene = session.createCriteria(Gene.class)
-					.add(Restrictions.eq("geneId", gene.getGeneId()))
-					.uniqueResult();
-			if (oldGene == null) {
-				session.save(gene);
-				session.flush(); // to get the PK
-			} else {
-				Gene geneToUpdate = (Gene) oldGene;
-				geneToUpdate.update(gene);
-				session.update(geneToUpdate);
-				gene = geneToUpdate;
-			}
-			
-			// Inserta Transcript (o recupera su id. si ya existe)
-			transcript.setGeneId(gene.getPk());
-			Object oldTranscript = session.createCriteria(Transcript.class)
-					.add(Restrictions.eq("transcriptID", transcript.getTranscriptID()))
-					.uniqueResult();
-			if (oldTranscript == null) {
-				session.save(transcript);
-				session.flush(); // to get the PK
-			} else {
-				Transcript transcriptToUpdate = (Transcript) oldTranscript;
-				transcriptToUpdate.update(transcript);
-				session.update(transcriptToUpdate);
-				transcript = transcriptToUpdate;
-			}
-			
-			target.setTranscript_pk(transcript.getPk());
-			session.save(target);
+		Object oldMiRna = session.createCriteria(MiRna.class)
+				.add(Restrictions.eq("name", miRna.getName()))
+				.uniqueResult();
+		if (oldMiRna == null) {
+			System.out.println("SALVANDO:");
+			System.out.println(miRna);
+			session.save(miRna);
 			session.flush(); // to get the PK
-			
-			// Inserta nueva DataExpression
-			// (y la relaciona con el MiRna y Target)
-			
-			id.setMirna_pk(miRna.getPk());
-			id.setTarget_pk(target.getPk());
-			id.setGene_pk(gene.getPk());
-			session.save(id);
-			session.flush(); // to get the PK
-			
-			count++;
-			if (count%100==0) {
-				System.out.println(count);
-				session.flush();
-		        session.clear();
-			}
-			
-			}
-			stmt.close();
-			tx.commit();
-			
-		} catch (SQLException e) {
-			tx.rollback();
-			e.printStackTrace();
-		} finally {
-			if (con!=null) con.close();
-			HibernateUtil.closeSession();
-			HibernateUtil.closeSessionFactory();
+		} else {
+			MiRna miRnaToUpdate = (MiRna) oldMiRna;
+			miRnaToUpdate.update(miRna);
+			session.update(miRnaToUpdate);
+			miRna = miRnaToUpdate;
 		}
-
+		
+		Object oldGene = session.createCriteria(Gene.class)
+				.add(Restrictions.eq("geneId", gene.getGeneId()))
+				.uniqueResult();
+		if (oldGene == null) {
+			session.save(gene);
+			session.flush(); // to get the PK
+		} else {
+			Gene geneToUpdate = (Gene) oldGene;
+			geneToUpdate.update(gene);
+			session.update(geneToUpdate);
+			gene = geneToUpdate;
+		}
+		
+		// Inserta Transcript (o recupera su id. si ya existe)
+		transcript.setGeneId(gene.getPk());
+		Object oldTranscript = session.createCriteria(Transcript.class)
+				.add(Restrictions.eq("transcriptID", transcript.getTranscriptID()))
+				.uniqueResult();
+		if (oldTranscript == null) {
+			session.save(transcript);
+			session.flush(); // to get the PK
+		} else {
+			Transcript transcriptToUpdate = (Transcript) oldTranscript;
+			transcriptToUpdate.update(transcript);
+			session.update(transcriptToUpdate);
+			transcript = transcriptToUpdate;
+		}
+		
+		target.setTranscript_pk(transcript.getPk());
+		session.save(target);
+		session.flush(); // to get the PK
+		
+		// Inserta nueva DataExpression
+		// (y la relaciona con el MiRna y Target)
+		
+		id.setMirna_pk(miRna.getPk());
+		id.setTarget_pk(target.getPk());
+		id.setGene_pk(gene.getPk());
+		session.save(id);
+		session.flush(); // to get the PK
+		
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		
 		Microcosm microcosm = new Microcosm();
 		
-		//1
-		//String inputFile = "/Users/esteban/Softw/miRNA/microcosm/v5.txt.homo_sapiens";
-		//microcosm.insertInTable(inputFile);
+		// /* 1. meter datos en mirna_raw */
+		// String inputFile = "/Users/esteban/Softw/miRNA/microcosm/v5.txt.homo_sapiens";
+		// microcosm.insertInTable(inputFile);
+		
+		/* 2. meter datos en mirna */
 		microcosm.insertIntoSQLModel();
 		
 	}
--- miRNA/src/mirna/database/microT_CDS_data.java
+++ miRNA/src/mirna/database/microT_CDS_data.java
@@ -5,7 +5,6 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import mirna.beans.Gene;
@@ -14,19 +13,22 @@ import mirna.beans.MiRna;
 import mirna.beans.Target;
 import mirna.beans.Transcript;
 import mirna.exception.MiRnaException;
-import mirna.utils.HibernateUtil;
 
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
-public class microT_CDS_data extends MirnaDatabase {
+public class microT_CDS_data extends NewMirnaDatabase {
 
 	private final String tableName = "microt_cds";
 
 	public microT_CDS_data() throws MiRnaException {
 		super();
+		this.fetchSizeMin = true;
+	}
+	
+	protected String getTableName() {
+		return tableName;
 	}
 
 	public void insertInTable(String csvInputFile) throws Exception {
@@ -117,154 +119,111 @@ public class microT_CDS_data extends MirnaDatabase {
 
 	}
 
+	
 	@Override
-	public void insertIntoSQLModel() throws Exception {
-
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		Connection con = null;
-		Transaction tx = session.beginTransaction();
-
-		try {
-			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-			Statement stmt = (Statement) con.createStatement();
-			stmt.setFetchSize(Integer.MIN_VALUE);
-
-			// our SQL SELECT query.
-			// if you only need a few columns, specify them by name instead of
-			// using "*"
-			String query = "SELECT * FROM " + tableName;
-			System.out.println("STARTING: " + query);
-
-			// execute the query, and get a java resultset
-			ResultSet rs = stmt.executeQuery(query);
-
-			// iterate through the java resultset
-			int count = 0;
-
-			while(rs.next() && count<2){
-			
-			// CAMBIAR ESTO:
-
-			String transcriptId = rs.getString("transcript_id");
-			String geneId = rs.getString("gene_id");
-			String miRNA = rs.getString("miRNA");
-			String miTG_score = rs.getString("miTG_score");
-			String region = rs.getString("region");
-			String chromosome = rs.getString("chromosome");
-			String coordinates = rs.getString("coordinates");
-
-			MiRna miRna = new MiRna();
-			miRna.setName(miRNA);
-
-			Gene gene = new Gene();
-			gene.setName(geneId);
-
-			InteractionData id = new InteractionData();
-			id.setMiTG_score(miTG_score);
-			id.setProvenance("microT-CDS");
-
-			Target target = new Target();
-			target.setRegion(region);
-			target.setChromosome(chromosome);
-			target.setCoordinates(coordinates);
-
-			Transcript transcript = new Transcript();
-			transcript.setTranscriptID(transcriptId);
-
-			/*
-			 * System.out.println(miRna); System.out.println(gene);
-			 * System.out.println(id); System.out.println(target);
-			 * System.out.println(transcript);
-			 */
-
-			// Inserta MiRna (o recupera su id. si ya existe)
-			Object oldMiRna = session.createCriteria(MiRna.class)
-					.add(Restrictions.eq("name", miRna.getName()))
-					.uniqueResult();
-			if (oldMiRna == null) {
-				session.save(miRna);
-				session.flush(); // to get the PK
-			} else {
-				MiRna miRnaToUpdate = (MiRna) oldMiRna;
-				miRnaToUpdate.update(miRna);
-				session.update(miRnaToUpdate);
-				miRna = miRnaToUpdate;
-			}
-
-			// Inserta gene (o recupera su id. si ya existe)
-			Object oldGene = session.createCriteria(Gene.class)
-					.add(Restrictions.eq("name", gene.getName()))
-					.uniqueResult();
-			if (oldGene == null) {
-				session.save(gene);
-				session.flush(); // to get the PK
-			} else {
-				Gene geneToUpdate = (Gene) oldGene;
-				geneToUpdate.update(gene);
-				session.update(geneToUpdate);
-				gene = geneToUpdate;
-			}
-			
-			transcript.setGeneId(gene.getPk());
-			
-			// Inserta Transcript (o recupera su id. si ya existe)
-			Object oldTranscript = session.createCriteria(Transcript.class)
-					.add(Restrictions.eq("transcriptID", transcript.getTranscriptID()))
-					.uniqueResult();
-			if (oldTranscript == null) {
-				session.save(transcript);
-				session.flush(); // to get the PK
-			} else {
-				Transcript transcriptToUpdate = (Transcript) oldTranscript;
-				transcriptToUpdate.update(transcript);
-				session.update(transcriptToUpdate);
-				transcript = transcriptToUpdate;
-			}
-
-			// Inserta Target
-			target.setTranscript_pk(transcript.getPk());
-			session.save(target);
-			session.flush(); // to get the PK
-
-			//Inserta nueva interactionData
-			// (y la relaciona con el MiRna y Target correspondiente)
+	protected void processRow(Session session, ResultSet rs) throws Exception {
 		
-			id.setTarget_pk(target.getPk());
-			id.setMirna_pk(miRna.getPk());
-			id.setGene_pk(gene.getPk());
-			session.save(id);
-			session.flush();
-			
-			count++;
-			if (count%100==0) {
-				System.out.println(count);
-				session.flush();
-		        session.clear();
-			}
+		String transcriptId = rs.getString("transcript_id");
+		String geneId = rs.getString("gene_id");
+		String miRNA = rs.getString("miRNA");
+		String miTG_score = rs.getString("miTG_score");
+		String region = rs.getString("region");
+		String chromosome = rs.getString("chromosome");
+		String coordinates = rs.getString("coordinates");
 
-			}
-			stmt.close();
-			tx.commit();
-			
-		} catch (SQLException e) {
-			tx.rollback();
-			e.printStackTrace();
-		} finally {
-			if (con!=null) con.close();
-			HibernateUtil.closeSession();
-			HibernateUtil.closeSessionFactory();
+		MiRna miRna = new MiRna();
+		miRna.setName(miRNA);
+
+		Gene gene = new Gene();
+		gene.setName(geneId);
+
+		InteractionData id = new InteractionData();
+		id.setMiTG_score(miTG_score);
+		id.setProvenance("microT-CDS");
+
+		Target target = new Target();
+		target.setRegion(region);
+		target.setChromosome(chromosome);
+		target.setCoordinates(coordinates);
+
+		Transcript transcript = new Transcript();
+		transcript.setTranscriptID(transcriptId);
+
+		/*
+		 * System.out.println(miRna); System.out.println(gene);
+		 * System.out.println(id); System.out.println(target);
+		 * System.out.println(transcript);
+		 */
+
+		// Inserta MiRna (o recupera su id. si ya existe)
+		Object oldMiRna = session.createCriteria(MiRna.class)
+				.add(Restrictions.eq("name", miRna.getName()))
+				.uniqueResult();
+		if (oldMiRna == null) {
+			session.save(miRna);
+			session.flush(); // to get the PK
+		} else {
+			MiRna miRnaToUpdate = (MiRna) oldMiRna;
+			miRnaToUpdate.update(miRna);
+			session.update(miRnaToUpdate);
+			miRna = miRnaToUpdate;
 		}
 
-	}
+		// Inserta gene (o recupera su id. si ya existe)
+		Object oldGene = session.createCriteria(Gene.class)
+				.add(Restrictions.eq("name", gene.getName()))
+				.uniqueResult();
+		if (oldGene == null) {
+			session.save(gene);
+			session.flush(); // to get the PK
+		} else {
+			Gene geneToUpdate = (Gene) oldGene;
+			geneToUpdate.update(gene);
+			session.update(geneToUpdate);
+			gene = geneToUpdate;
+		}
+		
+		transcript.setGeneId(gene.getPk());
+		
+		// Inserta Transcript (o recupera su id. si ya existe)
+		Object oldTranscript = session.createCriteria(Transcript.class)
+				.add(Restrictions.eq("transcriptID", transcript.getTranscriptID()))
+				.uniqueResult();
+		if (oldTranscript == null) {
+			session.save(transcript);
+			session.flush(); // to get the PK
+		} else {
+			Transcript transcriptToUpdate = (Transcript) oldTranscript;
+			transcriptToUpdate.update(transcript);
+			session.update(transcriptToUpdate);
+			transcript = transcriptToUpdate;
+		}
 
+		// Inserta Target
+		target.setTranscript_pk(transcript.getPk());
+		session.save(target);
+		session.flush(); // to get the PK
+
+		//Inserta nueva interactionData
+		// (y la relaciona con el MiRna y Target correspondiente)
+	
+		id.setTarget_pk(target.getPk());
+		id.setMirna_pk(miRna.getPk());
+		id.setGene_pk(gene.getPk());
+		session.save(id);
+		session.flush();
+		
+	}
+	
 	public static void main(String[] args) throws Exception {
 
 		microT_CDS_data microT_CDS_data = new microT_CDS_data();
-
-		// String inputFile =
-		// "/Users/esteban/Softw/miRNA/microalgo/microT_CDS_data.csv";
+		
+		// /* 1. meter datos en mirna_raw */
+		// String inputFile = "/Users/esteban/Softw/miRNA/microalgo/microT_CDS_data.csv";
 		// microT_CDS_data.insertInTable(inputFile);
-
+				
+		/* 2. meter datos en mirna */
 		microT_CDS_data.insertIntoSQLModel();
 
 	}
