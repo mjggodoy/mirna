0ad5a9f3a6df4ceacb303c4a564cc35e2923305c
María Jesús García Godoy <mjggodoy@gmail.com>
2015-09-24 13:04:56 +0200

    The same action than the latest push.

--- miRNA/src/mirna/database/MirDIP.java
+++ miRNA/src/mirna/database/MirDIP.java
@@ -5,18 +5,13 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-
 import mirna.beans.Gene;
 import mirna.beans.InteractionData;
 import mirna.beans.MiRna;
 import mirna.exception.MiRnaException;
-import mirna.utils.HibernateUtil;
-
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 /**
@@ -25,40 +20,40 @@ import org.hibernate.criterion.Restrictions;
  * @author Esteban López Camacho
  *
  */
-public class mirDIP extends MirnaDatabase {
-	
-	private final String tableName = "mirDIP";
-	
-	public mirDIP() throws MiRnaException {
-		super();
+public class MirDIP extends NewMirnaDatabase {
+
+	private  static final String TABLE_NAME = "mirDIP";
+
+	public MirDIP() throws MiRnaException {
+		super(TABLE_NAME);
 	}
-	
+
 	public void insertInTable(String csvInputFile) throws Exception {
-		
+
 		Connection con = null;
 		String line = null;
 		String[] tokens = null;
-		
+
 		try {
 			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
 			Statement stmt = (Statement) con.createStatement();
-			
+
 			FileReader fr = new FileReader(csvInputFile);
 			BufferedReader br = new BufferedReader(fr);
-	
+
 			int count = 0;
-	
+
 			br.readLine();
-			
+
 			while (((line = br.readLine()) != null)) {
-	
+
 				count++;
 				System.out.println(count);
-				
+
 				tokens = StringUtils.splitPreserveAllTokens(line, "\t");
-	
+
 				if (line != null) {
-					
+
 					String microrna = tokens[0].replaceAll("'", "\\\\'");
 					String gene_symbol = tokens[1].replaceAll("'", "\\\\'");
 					String source = tokens[2].replaceAll("'", "\\\\'");
@@ -69,11 +64,11 @@ public class mirDIP extends MirnaDatabase {
 							+ gene_symbol + "','"
 							+ source + "','"
 							+ rank + "')";
-					
+
 					stmt.executeUpdate(query);
-	
+
 				}
-	
+
 			}
 			fr.close();
 			br.close();
@@ -90,120 +85,67 @@ public class mirDIP extends MirnaDatabase {
 		} finally {
 			if (con!=null) con.close();
 		}
-		
+
 	}
-	
+
 	@Override
-	public void insertIntoSQLModel()
-			throws Exception {
-		
-		Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
+	public void processRow(Session session, ResultSet rs) throws Exception {
 
-		Connection con = null;
-		
-		try {
-			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-			
-			Statement stmt = con.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, 
-					java.sql.ResultSet.CONCUR_READ_ONLY); 
-			stmt.setFetchSize(Integer.MIN_VALUE); 
-			
-			// our SQL SELECT query. 
-			// if you only need a few columns, specify them by name instead of using "*"
-			String query = "SELECT * FROM " + tableName;
-			System.out.println("STARTING: " + query);
-			
-			// execute the query, and get a java resultset
-			ResultSet rs = stmt.executeQuery(query);
-			
-			// iterate through the java resultset
-			int count = 0;
-			
-			// CAMBIAR ESTO:
-			rs.next();
-			
-			
-			String microrna = rs.getString("microrna");
-			String geneSymbol = rs.getString("gene_symbol");
-			String source = rs.getString("source");
-			String rank = rs.getString("rank");
-			
-			MiRna miRna = new MiRna();
-			miRna.setName(microrna);
-			
-			Gene gene = new Gene();
-			gene.setName(geneSymbol);
+		String microrna = rs.getString("microrna");
+		String geneSymbol = rs.getString("gene_symbol");
+		String source = rs.getString("source");
+		String rank = rs.getString("rank");
 
-			InteractionData id = new InteractionData();
-			id.setRank(rank);
-			id.setProvenance("mirDIP (" + source + ")");
-			
-			// Inserta MiRna (o recupera su id. si ya existe)
-			Object oldMiRna = session.createCriteria(MiRna.class)
-					.add( Restrictions.eq("name", miRna.getName()) )
-					.uniqueResult();
-			if (oldMiRna==null) {
-				session.save(miRna);
-				session.flush();  // to get the PK
-			} else {
-				MiRna miRnaToUpdate = (MiRna) oldMiRna;
-				miRnaToUpdate.update(miRna);
-				session.update(miRnaToUpdate);
-				miRna = miRnaToUpdate;
-			}
-			
-			// Inserta gene (o recupera su id. si ya existe)
-			Object oldGene = session.createCriteria(Gene.class)
-					.add(Restrictions.eq("name", gene.getName()))
-					.uniqueResult();
-			if (oldGene == null) {
-				session.save(gene);
-				session.flush(); // to get the PK
-			} else {
-				Gene geneToUpdate = (Gene) oldGene;
-				geneToUpdate.update(gene);
-				session.update(geneToUpdate);
-				gene = geneToUpdate;
-			}
-			
-			id.setMirna_pk(miRna.getPk());
-			id.setGene_pk(gene.getPk());
-			
-			session.save(id);
-			session.flush();
-					
-			count++;
-			if (count%100==0) {
-				session.flush();
-		        session.clear();
-			}
-			
-			rs.close();
-			stmt.close();
-			tx.commit();
-			
-		} catch (SQLException e) {
-			tx.rollback();
-			e.printStackTrace();
+		MiRna miRna = new MiRna();
+		miRna.setName(microrna);
 
-		} finally {
-			if (con!=null) con.close();
-			HibernateUtil.closeSession();
-			HibernateUtil.closeSessionFactory();
+		Gene gene = new Gene();
+		gene.setName(geneSymbol);
+
+		InteractionData id = new InteractionData();
+		id.setRank(rank);
+		id.setProvenance("mirDIP (" + source + ")");
+
+		// Inserta MiRna (o recupera su id. si ya existe)
+		Object oldMiRna = session.createCriteria(MiRna.class)
+				.add( Restrictions.eq("name", miRna.getName()) )
+				.uniqueResult();
+		if (oldMiRna==null) {
+			session.save(miRna);
+			session.flush();  // to get the PK
+		} else {
+			MiRna miRnaToUpdate = (MiRna) oldMiRna;
+			miRnaToUpdate.update(miRna);
+			session.update(miRnaToUpdate);
+			miRna = miRnaToUpdate;
 		}
-		
+
+		// Inserta gene (o recupera su id. si ya existe)
+		Object oldGene = session.createCriteria(Gene.class)
+				.add(Restrictions.eq("name", gene.getName()))
+				.uniqueResult();
+		if (oldGene == null) {
+			session.save(gene);
+			session.flush(); // to get the PK
+		} else {
+			Gene geneToUpdate = (Gene) oldGene;
+			geneToUpdate.update(gene);
+			session.update(geneToUpdate);
+			gene = geneToUpdate;
+		}
+
+		id.setMirna_pk(miRna.getPk());
+		id.setGene_pk(gene.getPk());	
+		session.save(id);
+		session.flush();
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-		
-		mirDIP mirdip = new mirDIP();
-		
-		//String inputFile = "C:/Users/Esteban/Desktop/temp/mirDIP-All-Data-Version1.0.txt";
-		//mirdip.insertInTable(inputFile);
-		
+
+		MirDIP mirdip = new MirDIP();
 		mirdip.insertIntoSQLModel();
-		
+
 	}
 
 }
