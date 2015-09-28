cf77aa96e6421476eb72eb6271968cfc91d271e1
María Jesús García Godoy <mjggodoy@gmail.com>
2015-09-28 14:46:12 +0200

    Some changes that don't affect the whole thing: just testing

--- miRNA/src/mirna/beans/SmallMolecule.java
+++ miRNA/src/mirna/beans/SmallMolecule.java
@@ -14,23 +14,23 @@ import mirna.exception.ConflictException;
 
 public class SmallMolecule extends ModelClass {
 
-	
+
 	@Column(name = "fda", nullable = false, length = 300)
 	private String fda;
-	
+
 	@Column(name = "db", nullable = false, length = 300)
 	private String db;
-	
+
 	@Column(name = "cid", nullable = false, length = 300)
 	private String cid;
-	
+
 	@Column(name = "environmental_factor_pk", nullable = false, length = 20)
 	private Integer environmental_factor_pk;
-	
+
 
 	public SmallMolecule() { }
 
-	
+
 	public String getFda() {
 		return fda;
 	}
@@ -54,8 +54,8 @@ public class SmallMolecule extends ModelClass {
 	public void setCid(String cid) {
 		this.cid = cid;
 	}
-	
-	
+
+
 	public Integer getEnvironmental_factor_pk() {
 		return environmental_factor_pk;
 	}
@@ -69,41 +69,51 @@ public class SmallMolecule extends ModelClass {
 
 	public int checkConflict(SmallMolecule smallmolecule) {
 		int res = 0;
-		
+
 		if (this.pk!=null) {
 			if (smallmolecule.getPk()==null) res++; // res = 1
-			else if (!this.pk.equals(smallmolecule.getPk())) return -1;
+			else if (!this.pk.equals(smallmolecule.getPk())) { 
+				return -1;
+			}
 		}
-		
+
 		if (this.fda!=null) {
 			if (smallmolecule.getFda()==null) res++; // res = 1
-			else if (!this.fda.equals(smallmolecule.getFda())) return -1;
+			else if (!this.fda.equals(smallmolecule.getFda())){ 
+				return -1;
+			};
 		}
-		
+
 		if (this.cid!=null) {
 			if (smallmolecule.getCid()==null) res++; // res = 1
-			else if (!this.cid.equals(smallmolecule.getCid())) return -1;
+			else if (!this.cid.equals(smallmolecule.getCid())) { 
+				return -1;
+			}
 		}
-		
+
 		if (this.db!=null) {
 			if (smallmolecule.getDb()==null) res++; // res = 1
-			else if (!this.db.equals(smallmolecule.getDb())) return -1;
+			else if (!this.db.equals(smallmolecule.getDb())) { 
+				return -1;
+			}
 		}
-		
+
 		if (this.environmental_factor_pk!=null) {
 			if (smallmolecule.getEnvironmental_factor_pk()==null) res++; // res = 1
-			else if (!this.environmental_factor_pk.equals(smallmolecule.getEnvironmental_factor_pk())) return -1;
+			else if (!this.environmental_factor_pk.equals(smallmolecule.getEnvironmental_factor_pk())) { 
+				return -1;
+			}
 		}
-		
+
 		return res;
 	}
-	
-	
+
+
 	public void update(SmallMolecule smallmolecule) throws ConflictException {
 		this.update(smallmolecule, true);
 	}
-	
-	
+
+
 	public void update(SmallMolecule smallmolecule, boolean checkConflict) throws ConflictException {
 		if (checkConflict) {
 			if (this.checkConflict(smallmolecule)==-1) throw new ConflictException(this, smallmolecule);
@@ -115,7 +125,7 @@ public class SmallMolecule extends ModelClass {
 		if (smallmolecule.getEnvironmental_factor_pk()!=null) this.environmental_factor_pk = smallmolecule.getEnvironmental_factor_pk();
 
 
-		
+
 	}
 
 
@@ -126,9 +136,9 @@ public class SmallMolecule extends ModelClass {
 				+ ", pk=" + pk + "]";
 	}
 
-	
 
-	
-	
-	
+
+
+
+
 }
