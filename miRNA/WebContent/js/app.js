df89991ab99e31e61e7da7c21c65583479d6e4cb
Esteban López Camacho <esteban@lcc.uma.es>
2015-11-26 19:16:08 +0100

    Primeras páginas iniciales de la interfaz.

--- miRNA/WebContent/js/app.js
+++ miRNA/WebContent/js/app.js
@@ -1,21 +1,23 @@
 angular.module('mirna', ['ui.router', 'ngResource', 'spring-data-rest', 'mirna.controllers', 'mirna.services']);
 
 angular.module('mirna').config(function($stateProvider, $locationProvider) {
-  
-  $stateProvider.state('listMirna', { // state for showing all mirnas
-    url: '/mirna',
-    templateUrl: 'partials/mirna-list.html',
-    controller: 'MirnaListController'
-  }).state('viewMirna', { //state for showing single mirna
-    url: '/mirna/:id',
-    templateUrl: 'partials/mirna-view.html',
-    controller: 'MirnaViewController'
-  }).state('home', { //home state
-    url: '/home',
-    templateUrl: 'partials/home.html'
-  });
-    
+	
+	$stateProvider.state('listMirna', { // state for showing all mirnas
+		url: '/mirna',
+		templateUrl: 'partials/mirna-list.html',
+		controller: 'MirnaListController'
+	}).state('viewMirna', { //state for showing single mirna
+		url: '/mirna/:id',
+		templateUrl: 'partials/mirna-view.html',
+		controller: 'MirnaViewController'
+	}).state('search', { //search state
+		url: '/search',
+		templateUrl: 'partials/search.html'
+	}).state('home', { //home state
+		url: '/home',
+		templateUrl: 'partials/home.html'
+	});
+
 }).run(function($state) {
-  $state.go('home'); //make a transition to movies state when app starts
-  //console.log($state);
+	$state.go('home'); //make a transition to movies state when app starts
 });
