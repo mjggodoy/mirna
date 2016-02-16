eff6aaaeb4952c86bcff8745de5f3d34ad27022f
Esteban López Camacho <esteban@lcc.uma.es>
2016-02-16 13:29:35 +0100

    Búsqueda de Minas relacionados con phenotypes.

--- miRNA/src/main/java/mirna/api/repo/ExpressionDataRepository.java
+++ miRNA/src/main/java/mirna/api/repo/ExpressionDataRepository.java
@@ -8,14 +8,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import mirna.api.model.ExpressionData;
-import mirna.api.model.projection.InlineDisease;
 
-
-@RepositoryRestResource(collectionResourceRel = "expression_data", path = "expression_data", excerptProjection = InlineDisease.class)
+@RepositoryRestResource(collectionResourceRel = "expression_data", path = "expression_data")
 public interface ExpressionDataRepository extends PagingAndSortingRepository<ExpressionData, Integer> {
 	
 	public Page<ExpressionData> findByProvenance(@Param("provenance")String provenance, Pageable pageable);
-	//public Page<ExpressionData> findBy(@Param("mirna_pk")String mirna_pk, Pageable pageable);
 	
 	@RestResource(path = "mirna_pk")
 	public Page<ExpressionData> findByMirnas_Pk(@Param("pk")int pk, Pageable pageable);
--- miRNA/src/main/java/mirna/api/repo/MirnaRepository.java
+++ miRNA/src/main/java/mirna/api/repo/MirnaRepository.java
@@ -1,7 +1,10 @@
 package mirna.api.repo;
 
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,12 +12,14 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import mirna.api.model.MiRna;
 
-
 @RepositoryRestResource(collectionResourceRel = "mirna", path = "mirna")
-
 public interface MirnaRepository extends PagingAndSortingRepository<MiRna, Integer> {
 	
 	@RestResource(path = "id")
 	public Page<MiRna> findByIdContainingIgnoreCase(@Param("id")String id, Pageable pageable);
+	
+	@Query("SELECT a from MiRna a, MirnaPkTranslation b, ExpressionData c where c.mirnaPk=b.oldPk and b.newPk=a.pk and c.disease.pk=:key")	
+	@RestResource(path = "related_to_disease")
+	public Set<MiRna> findMirnasRelatedToDisease(@Param("key")int pk);
 	
 }
