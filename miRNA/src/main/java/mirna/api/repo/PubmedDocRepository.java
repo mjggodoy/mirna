9e5999210fd940b4d968c6d0e3a6324a577ccd1d
Esteban López Camacho <esteban@lcc.uma.es>
2016-02-24 18:36:21 +0100

    Fixed PubmedDocumentRepository

--- miRNA/src/main/java/mirna/api/repo/PubmedDocRepository.java
+++ miRNA/src/main/java/mirna/api/repo/PubmedDocRepository.java
@@ -9,13 +9,11 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import mirna.api.model.PubmedDocument;
 
-
 @RepositoryRestResource(collectionResourceRel = "pubmed_document", path = "pubmed_document")
-
 public interface PubmedDocRepository extends PagingAndSortingRepository<PubmedDocument, Integer> {
 	
-	/*@RestResource(path = "mirna_pk")
-	public Page<PubmedDocument> findByMirnas_Pk(@Param("pk")int pk, Pageable pageable);*/
+	@RestResource(path = "mirna_pk")
+	public Page<PubmedDocument> findByMirnas_Pk(@Param("pk")int pk, Pageable pageable);
 	
 	@RestResource(path = "id")
     public Page<PubmedDocument> findById(@Param("id")String id, Pageable pageable);
