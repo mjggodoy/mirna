5310d10807129e0f296a9eaa62aa47a441f4bf63
Esteban López Camacho <esteban@lcc.uma.es>
2016-02-25 14:09:38 +0100

    Minor fixes to indentation

--- miRNA/src/main/java/mirna/api/repo/PubmedDocRepository.java
+++ miRNA/src/main/java/mirna/api/repo/PubmedDocRepository.java
@@ -13,11 +13,11 @@ import mirna.api.model.PubmedDocument;
 @RepositoryRestResource(collectionResourceRel = "pubmed_document", path = "pubmed_document")
 
 public interface PubmedDocRepository extends PagingAndSortingRepository<PubmedDocument, Integer> {
-
-    @RestResource(path = "mirna_pk")
-    public Page<PubmedDocument> findByMirnas_Pk(@Param("pk")int pk, Pageable pageable);
+	
+	@RestResource(path = "mirna_pk")
+	public Page<PubmedDocument> findByMirnas_Pk(@Param("pk")int pk, Pageable pageable);
 	
 	@RestResource(path = "id")
-    public Page<PubmedDocument> findById(@Param("id")String id, Pageable pageable);
-	
+	public Page<PubmedDocument> findById(@Param("id")String id, Pageable pageable);
+
 }
