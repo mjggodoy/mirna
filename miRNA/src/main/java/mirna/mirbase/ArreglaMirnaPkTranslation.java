0919884187029eefac68cfae3c0bc8d1a0c60be5
Esteban López Camacho <esteban@lcc.uma.es>
2016-06-28 12:54:32 +0200

    All cases from mirbase taken in consideration

--- miRNA/src/main/java/mirna/mirbase/ArreglaMirnaPkTranslation.java
+++ miRNA/src/main/java/mirna/mirbase/ArreglaMirnaPkTranslation.java
@@ -25,10 +25,130 @@ public class ArreglaMirnaPkTranslation {
 		this.dbPassword = props.getProperty("password");
 	}
 
-	public void execute() throws Exception {
+	public void setMatures() throws Exception {
 
 		Statement stmt = null;
 		ResultSet rs = null;
+
+		List<Integer> pks = new ArrayList<Integer>();
+
+		try {
+
+			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+			stmt = (Statement) con.createStatement();
+
+			String query = "select a.pk from mirna.mirna2 a, mirna.mirna_pk_translation b, mirna_old.mirna c where a.pk=b.new_pk and b.old_pk=c.pk and c.arm is not null and a.mirbase_pk is null";
+			System.out.println("STARTING: " + query);
+
+			// execute the query, and get a java resultset
+			rs = stmt.executeQuery(query);
+
+			int limit = -1;
+			int counter = 0;
+
+			while (rs.next() && limit!=0) {
+				pks.add(rs.getInt("pk"));
+			}
+
+			System.out.println(pks.size());
+
+			for (int pk : pks) {
+				setTypeToMature(pk);
+			}
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs!=null) rs.close();
+			if (stmt!=null) stmt.close();
+			if (con!=null) con.close();
+		}
+	}
+
+	private void setTypeToMature(int pk) throws Exception {
+
+		String query = "update mirna.mirna2 set type=? "
+				+ "where pk=?";
+
+		PreparedStatement stmt = null;
+
+		try {
+
+			stmt = con.prepareStatement(query);
+			stmt.setString(1, "mature");
+			stmt.setInt(2, pk);
+			stmt.execute();
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (stmt!=null) stmt.close();
+		}
+	}
+
+	public void setArmsNotMirbase() throws Exception {
+
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+			stmt = (Statement) con.createStatement();
+
+			String query = "select a.pk, c.arm from mirna.mirna2 a, mirna.mirna_pk_translation b, mirna_old.mirna c where a.pk=b.new_pk and b.old_pk=c.pk and c.arm is not null and a.mirbase_pk is null";
+			System.out.println("STARTING: " + query);
+
+			// execute the query, and get a java resultset
+			rs = stmt.executeQuery(query);
+
+			int limit = -1;
+			int counter = 0;
+
+			while (rs.next() && limit!=0) {
+				int pk = rs.getInt("pk");
+				String arm = rs.getString("arm");
+				setArmToMature(pk, arm);
+			}
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs!=null) rs.close();
+			if (stmt!=null) stmt.close();
+			if (con!=null) con.close();
+		}
+	}
+
+	private void setArmToMature(int pk, String arm) throws Exception {
+
+		String query = "update mirna.mirna2 set arm=? "
+				+ "where pk=?";
+
+		PreparedStatement stmt = null;
+
+		try {
+
+			stmt = con.prepareStatement(query);
+			stmt.setString(1, arm);
+			stmt.setInt(2, pk);
+			stmt.execute();
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (stmt!=null) stmt.close();
+		}
+	}
+
+	public void recorreMirnaConArmNotNull() throws Exception {
+
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		List<Integer> pks = new ArrayList<Integer>();
 
 		try {
 
@@ -43,18 +163,36 @@ public class ArreglaMirnaPkTranslation {
 			rs = stmt.executeQuery(query);
 
 			int limit = -1;
-			int counter = 0;
 
 			while (rs.next() && limit!=0) {
-
 				int oldPk = rs.getInt("pk");
 				String arm = rs.getString("arm");
-
-				String mirna = buscaNuevo(oldPk);
-				if (!mirna.contains(arm)) {
-					mirna = buscaMejorNuevo();
+				List<QueryResult> mirnas = getTranslatedMirnas(oldPk);
+				int hairpins = 0;
+				int matures = 0;
+				for (QueryResult o : mirnas) {
+					if (o.type.equals("hairpin")) hairpins++;
+					if (o.type.equals("mature")) matures++;
 				}
-				syso(name + "-" + mirna);
+				boolean res = false;
+
+
+				if (matures==1 && (hairpins==0 || hairpins==1)) {
+					//ACTUALIZAR MATURE
+					res = true;
+				} else  if ((matures == 0) && (hairpins==0)) {
+					// ALL OK
+					res = true;
+
+				} else if ((matures>0) && (hairpins==0)) {
+					res = searchArmInMatures(mirnas, arm);
+				} else if ((matures==0) && (hairpins==1)) {
+					res = searchArmInMatures(getMatures(mirnas.get(0).pk), arm);
+				}
+
+				if (!res) {
+					System.err.println("Comprobar pk = "+oldPk+" hairpins("+hairpins+") matures("+matures+")");
+				}
 			}
 
 		} catch (SQLException e) {
@@ -66,36 +204,32 @@ public class ArreglaMirnaPkTranslation {
 		}
 	}
 
-	private String buscaNuevo(int oldPk) throws Exception {
-
+	private List<QueryResult> getTranslatedMirnas(int oldPk) throws SQLException {
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 
+		List<QueryResult> res = new ArrayList<>();
+
 		try {
 
-			String query = "select id from mirna.mirna2 "
-					+ "where "+column+" like '"+oldPk+",%' "
-					+ "or "+column+" like '%,"+oldPk+",%'";
-			stmt = con.prepareStatement(query);
-//			stmt.setInt(1, oldPk);
-//			stmt.setInt(2, oldPk);
+			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+			String query = "select a.pk, a.id, a.type from mirna.mirna2 a, mirna.mirna_pk_translation b where a.pk=b.new_pk and b.old_pk=? and a.mirbase_pk is not null";
 
 			// execute the query, and get a java resultset
+			stmt = con.prepareStatement(query);
+			stmt.setInt(1, oldPk);
 			rs = stmt.executeQuery();
 
-			while (rs.next()) {
-				newPks.add(rs.getInt("pk"));
-			}
+			int limit = -1;
+			int counter = 0;
 
-			if (newPks.size()==0) { // TODO: Chequear >1
-				if (buscaEnPre) {
-					String exMsg = newPks.size()+" nuevos pks encontrados para oldPk="+oldPk+"\n"+query;
-					throw new Exception(exMsg);
-				} else {
-					newPks = buscaNuevoPk(oldPk, true);
-				}
-
-
+			while (rs.next() && limit!=0) {
+				QueryResult q = new QueryResult();
+				q.pk = rs.getInt("pk");
+				q.id = rs.getString("id");
+				q.type = rs.getString("type");
+				res.add(q);
 			}
 
 		} catch (SQLException e) {
@@ -103,9 +237,74 @@ public class ArreglaMirnaPkTranslation {
 		} finally {
 			if (rs!=null) rs.close();
 			if (stmt!=null) stmt.close();
+			if (con!=null) con.close();
 		}
 
-		return newPks;
+		return res;
+
+	}
+
+	private List<QueryResult> getMatures(int pk) throws SQLException {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		List<QueryResult> res = new ArrayList<>();
+
+		try {
+
+			con = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+
+			String query = "select c.* from mirna.hairpin2mature b, mirna.mirna2 c where b.hairpin_pk=? and b.mature_pk=c.pk;";
+
+			// execute the query, and get a java resultset
+			stmt = con.prepareStatement(query);
+			stmt.setInt(1, pk);
+			rs = stmt.executeQuery();
+
+			int limit = -1;
+			int counter = 0;
+
+			while (rs.next() && limit!=0) {
+				QueryResult q = new QueryResult();
+				q.pk = rs.getInt("pk");
+				q.id = rs.getString("id");
+				q.type = rs.getString("type");
+				res.add(q);
+			}
+
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs!=null) rs.close();
+			if (stmt!=null) stmt.close();
+			if (con!=null) con.close();
+		}
+
+		return res;
+
+	}
+
+	private boolean searchArmInMatures(List<QueryResult> matures, String arm) {
+		int counter=0;
+		for (QueryResult m : matures) {
+			if (m.id.contains(arm)) {
+				counter++;
+			}
+		}
+		return counter==1;
+	}
+
+	private class QueryResult {
+		int pk;
+		String id;
+		String type;
+	}
+
+	public static void main(String[] args) throws Exception {
+		ArreglaMirnaPkTranslation x = new ArreglaMirnaPkTranslation();
+		//x.setMatures();
+		//x.setArmsNotMirbase();
+		x.recorreMirnaConArmNotNull();
 	}
 
 
